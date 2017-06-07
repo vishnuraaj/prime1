6d2f7b01ce1c60e5b0f9e20b9f9a99d80ea3a642
vishnuraaj <vishnuraaj15@gmail.com>
2017-06-08 00:16:16 +0530

    Create stoi.java

---
+++ stoi.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class stoi
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+String str=s.next();
+int a=Integer.valueOf(str);
+System.out.println(a) ;
+}
+}
+
+
