fc8b3dfa365e9a44cb31c0049133c4056afe75c1
vishnuraaj <vishnuraaj15@gmail.com>
2017-06-10 21:37:57 +0530

    Create pattern.java

---
+++ pattern.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class pattern
+{
+public static void main(String args[])
+{
+int i,j;
+for(i=1;i<=6;i++)
+{
+for(j=1;j<i;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
