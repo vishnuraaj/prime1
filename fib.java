018bc1b9c618a46e413228536148fa929c080c8f
vishnuraaj <vishnuraaj15@gmail.com>
2017-06-10 22:18:49 +0530

    Create fib.java

---
+++ fib.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class fib
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+int n1=0,n2=1;n3,i,c;
+for(i=2;,i<c;++i){
+n3=n1+n2;
+System.out.print(n3);
+n1=n2;
+n2=n3;
+}}}
