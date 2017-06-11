2ad92f48e76b2759b32bec5aceb6909c56aaf3b8
vishnuraaj <vishnuraaj15@gmail.com>
2017-06-11 17:57:15 +0530

    Create revor.java

---
+++ revor.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class revor
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        String s1=s.nextLine();
+        for(int i=s1.length()-1;i>=0;i--)
+        {
+            System.out.print(s1+" ");
+        }
+    }
+}
