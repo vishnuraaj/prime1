ef26d0ca9775bb5eae6069d5b04cad8e9097a882
vishnuraaj <vishnuraaj15@gmail.com>
2017-06-18 22:11:08 +0530

    Create sumnu.java

---
+++ sumnu.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class sumnu
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a=0;
+        for(int i=0;i<=n;i++)
+        {
+            a+=i;
+        }
+        System.out.print(a);
+    }
+}
+
