8b5be22f6b3c192082278a1c34f31eb43ead64de
vishnuraaj <vishnuraaj15@gmail.com>
2017-06-18 21:49:09 +0530

    Create rvnu.java

---
+++ rvnu.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class rvnu
+{
+    public static void main(String args[])
+    {
+     int rev=0;   
+    Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+while(n>0)
+{
+    rev=rev*10;
+    rev=rev+n%10;
+    n=n/10;
+}
+   System.out.println(rev) ;
+}
+}
+
+
+
