93e4b607ccf0ffb558aba805c1a24ecac93204fb
vishnuraaj <vishnuraaj15@gmail.com>
2017-06-18 20:27:21 +0530

    Create pyramid.java

---
+++ pyramid.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class pyramid
+{
+    public static void main(String args[])
+    {
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print('*');
+            }
+        
+        System.out.println();
+        }
+    }
+}
